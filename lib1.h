be67937c61d6c7534038acdeba636317d5f0af3d
wilmer-45 <78442778+wilmer-45@users.noreply.github.com>
2022-03-28 17:57:46 -0500

    Create lib1.h

---
+++ lib1.h
@@ -0,0 +1,8 @@
+#ifndef LIB1_H
+#define LIB1_H
+
+char *read_file_to_buffer (const char *, unsigned int *);
+void free_buffer_mem(char *);
+void write_dArray_to_file (const double *, unsigned int , const char *);
+
+#endif
