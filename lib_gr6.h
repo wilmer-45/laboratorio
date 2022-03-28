bbd60d4b6330c63c5c868eb6e72f690e0f6d01c2
wilmer-45 <78442778+wilmer-45@users.noreply.github.com>
2022-03-28 17:58:41 -0500

    Create lib_gr6.h

---
+++ lib_gr6.h
@@ -0,0 +1,10 @@
+#ifndef LIB_GR6_H
+#define LIB_GR6_H
+
+void swap(double *, double *);
+void print(double *,int);
+void bubble(double *, int);
+void insert(double *, int );
+void printArr(double * ,int );
+
+#endif
